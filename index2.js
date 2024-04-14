c0348de9b0022ffc34bed2b70ee186762ba9fde2
Owlnhd <66353421+Owlnhd@users.noreply.github.com>
2024-04-14 22:41:56 +0900

    patch03

--- index2.js
+++ index2.js
@@ -4,6 +4,22 @@ const range = 'Sheet1'; // 데이터를 읽고 쓸 시트와 범위
 
 let dataList = []; // 데이터 확인 시 사용할 리스트 변수
 
+
+// // 현재 날짜와 시간을 가져오기
+// const currentDate = new Date();
+
+// // 각 구성 요소를 가져오기
+// const year = currentDate.getFullYear();
+// const month = currentDate.getMonth() + 1;
+// const day = currentDate.getDate();
+// const hours = currentDate.getHours();
+// const minutes = currentDate.getMinutes();
+// const seconds = currentDate.getSeconds();
+
+// // 날짜와 시간을 문자열로 포맷팅
+// const formatDate = `${year}${String(month).padStart(2, '0')}${String(day).padStart(2, '0')}${String(hours).padStart(2, '0')}${String(minutes).padStart(2, '0')}${String(seconds).padStart(2, '0')}`;
+
+
 // 사용자의 위치와 이름을 Google Sheets에 저장
 async function writeData() {
     if (!navigator.geolocation) {
@@ -18,13 +34,14 @@ async function writeData() {
     }
 
     navigator.geolocation.getCurrentPosition(async (position) => {
-        const latitude = Math.round(position.coords.latitude * 100000) / 100000;
-        const longitude = Math.round(position.coords.longitude * 100000) / 100000;
+        const latitude = position.coords.latitude.toFixed(5);
+        const longitude = position.coords.longitude.toFixed(5);
 
         const body = {
             name: name,
             latitude: latitude,
             longitude: longitude,
+            //time: formatDate
         };
 
         fetch('https://script.google.com/macros/s/AKfycbxR1tTrvqLH4dULybJTNUrvaQ8xbALBmyeXmmlb4P32j-PDbuC0W8U8vdTa6eZOrJqAIg/exec', {
@@ -54,18 +71,55 @@ function readData() {
         // 데이터를 순회하며 각 좌표별로 사람 수 계산
         data.values.forEach(row => {
             const name = row[2]; // 이름
-            const coords = row[0] + ',' + row[1]; // 위도와 경도를 콤마로 구분하여 문자열 생성
+            // const coords = row[0] + ',' + row[1]; // 위도와 경도를 콤마로 구분하여 문자열 생성
+            
+            const coords = new kakao.maps.LatLng(row[0], row[1]);
+
             counts[coords] = counts[coords] ? counts[coords] + 1 : 1;
-            dataList.push({name, coords}); // 리스트 변수에 저장
+            dataList.push({name, coords, counts}); // 리스트 변수에 저장
         });
 
         // 결과를 화면에 표시
         const messagesDiv = document.getElementById('messages');
         messagesDiv.innerHTML = '';
         Object.keys(counts).forEach(coords => {
-            messagesDiv.innerHTML += `좌표 [${coords}]에는 ${counts[coords]}명이 있습니다.<br>`;
+            messagesDiv.innerHTML += `좌표 ${coords}에는 ${counts[coords]}명이 있습니다.<br>`;
         });
         console.log(dataList[0].coords);
+
+
+        // dataList 참조하여 마커 생성 후 표시
+        dataList.forEach(element => {
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: element.coords,
+                image: markerImageRed
+            });
+        
+            let content = '<div class ="label">'
+            + '<span class="left"></span><span class="center">'
+            + counts[element.coords] + " 명이 있어요!" //+ element.population
+            + '</span><span class="right"></span></div>';
+        
+            let position = element.coords;
+        
+            let customOverlay = new kakao.maps.CustomOverlay({
+                position: position,
+                content: content,
+            });
+        
+            kakao.maps.event.addListener(marker, 'mouseover', function () {
+                customOverlay.setMap(map);
+            });
+        
+            kakao.maps.event.addListener(marker, 'mouseout', function () {
+                setTimeout(function () {
+                    customOverlay.setMap();
+                });
+            });
+        });
+
+
     })
     .catch(error => console.error('Error:', error));
 }
--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var map = new kakao.maps.Map(mapContainer, mapOption);
 // copyright의 위치를 오른쪽 아래로 설정하고, 로고와의 위치를 반전시킨다
 map.setCopyrightPosition(kakao.maps.CopyrightPosition.BOTTOMRIGHT, true);
 
+var locPosition;
+
 
 // 현재 위치 표시
 // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
@@ -19,9 +21,9 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         
         var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
+            lng = position.coords.longitude; // 경도
         
-        var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        locPosition = new kakao.maps.LatLng(lat, lng); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
 
         // // 사용자 마커 이미지의 이미지 속성입니다
         // var imageSrc = "images/user_image.png",
@@ -34,7 +36,7 @@ if (navigator.geolocation) {
         // 마커를 생성합니다
         var locUser = new kakao.maps.Marker({
             position: locPosition
-            //image: markerImage
+            // image: markerImage
         });
         
         // 마커를 지도 위에 표시합니다
@@ -68,6 +70,41 @@ if (navigator.geolocation) {
 }
 
 
+// dataList.forEach()
+// let dataPosition = new kakao.map.LatLng();
+
+
+dataList.forEach(element => {
+    let marker = new kakao.maps.Marker({
+        map: map,
+        position: element.coords,
+        image: markerImageRed
+    });
+
+    let content = '<div class ="label">'
+    + '<span class="left"></span><span class="center">'
+    + element.name //+ "    " + element.population
+    + '</span><span class="right"></span></div>';
+
+    let position = element.coords;
+
+    let customOverlay = new kakao.maps.CustomOverlay({
+        position: position,
+        content: content,
+    });
+
+    kakao.maps.event.addListener(marker, 'mouseover', function () {
+        customOverlay.setMap(map);
+    });
+
+    kakao.maps.event.addListener(marker, 'mouseout', function () {
+        setTimeout(function () {
+            customOverlay.setMap();
+        });
+    });
+});
+
+
 // 위치별 마커 생성 및 표시
 // 마커를 표시할 위치와 title 객체 배열입니다 
 var positions = [
@@ -88,28 +125,28 @@ var positions = [
     },
     {
         title: '장소4',
-        latlng: new kakao.maps.LatLng(36.799067, 127.074456),
+        latlng: new kakao.maps.LatLng(36.799057, 127.074306),
         population: 15
     }
 ];
 
-var red = [],
-    yellow = [],
-    green = [],
-    blue = [];
+var markerRed = [],
+    markerYellow = [],
+    markerGreen = [],
+    markerBlue = [];
 
 positions.forEach(element => {
     if (element.population >= 15) {
-        red.push(element);
+        markerRed.push(element);
     }
     else if (element.population >= 10) {
-        yellow.push(element);
+        markerYellow.push(element);
     }
     else if (element.population >= 5) {
-        green.push(element);
+        markerGreen.push(element);
     }
     else {
-        blue.push(element);
+        markerBlue.push(element);
     }
 });
 
@@ -129,17 +166,17 @@ const markerImageRed = new kakao.maps.MarkerImage(imageRed, imageSize),
     markerImageBlue = new kakao.maps.MarkerImage(imageBlue, imageSize);
 
 // 15명 이상 red marker
-red.forEach(element => {
+markerRed.forEach(element => {
     let marker = new kakao.maps.Marker({
         map: map,
         position: element.latlng,
         image: markerImageRed
     });
 
-    let content = '<div class ="label">' +
-    '<span class="left"></span>' + '<span class="center">' +
-    element.title + "    " + element.population +
-    '</span>' + '<span class="right"></span></div>';
+    let content = '<div class ="label">'
+    + '<span class="left"></span><span class="center">'
+    + element.title + "    " + element.population
+    + '</span><span class="right"></span></div>';
 
     let position = element.latlng;
 
@@ -160,17 +197,17 @@ red.forEach(element => {
 });
 
 // 10명 이상 yellow marker
-yellow.forEach(element => {
+markerYellow.forEach(element => {
     let marker = new kakao.maps.Marker({
         map: map,
         position: element.latlng,
         image: markerImageYellow
     });
 
-    let content = '<div class ="label">' +
-    '<span class="left"></span>' + '<span class="center">' +
-    element.title + "    " + element.population +
-    '</span>' + '<span class="right"></span></div>';
+    let content = '<div class ="label">'
+    + '<span class="left"></span><span class="center">'
+    + element.title + "    " + element.population
+    + '</span><span class="right"></span></div>';
 
     let position = element.latlng;
 
@@ -191,17 +228,17 @@ yellow.forEach(element => {
 });
 
 // 5명 이상 green marker
-green.forEach(element => {
+markerGreen.forEach(element => {
     let marker = new kakao.maps.Marker({
         map: map,
         position: element.latlng,
         image: markerImageGreen
     });
 
-    let content = '<div class ="label">' +
-    '<span class="left"></span>' + '<span class="center">' +
-    element.title + "    " + element.population +
-    '</span>' + '<span class="right"></span></div>';
+    let content = '<div class ="label">'
+    + '<span class="left"></span><span class="center">'
+    + element.title + "    " + element.population
+    + '</span><span class="right"></span></div>';
 
     let position = element.latlng;
 
@@ -222,17 +259,17 @@ green.forEach(element => {
 });
 
 // 5명 미만 blue marker
-blue.forEach(element => {
+markerBlue.forEach(element => {
     let marker = new kakao.maps.Marker({
         map: map,
         position: element.latlng,
         image: markerImageBlue
     });
 
-    let content = '<div class ="label">' +
-    '<span class="left"></span>' + '<span class="center">' +
-    element.title + "    " + element.population +
-    '</span>' + '<span class="right"></span></div>';
+    let content = '<div class ="label">'
+    + '<span class="left"></span><span class="center">'
+    + element.title + "    " + element.population
+    + '</span><span class="right"></span></div>';
 
     let position = element.latlng;
 
@@ -251,57 +288,4 @@ blue.forEach(element => {
         });
     });
 });
-
-// // 마커 이미지의 이미지 크기 입니다
-// var imageSize = new kakao.maps.Size(15, 15); 
-    
-// // 마커 이미지를 생성합니다    
-// let markerImageRed = new kakao.maps.MarkerImage(imageRed, imageSize),
-//     markerImageYellow = new kakao.maps.MarkerImage(imageYellow, imageSize),
-//     markerImageGreen = new kakao.maps.MarkerImage(imageGreen, imageSize),
-//     markerImageBlue = new kakao.maps.MarkerImage(imageBlue, imageSize);
-
-
-//     // 마커 이미지의 이미지 크기 입니다
-//     var imageSize = new kakao.maps.Size(15, 15); 
-    
-//     // 마커 이미지를 생성합니다    
-//     var markerImage = new kakao.maps.MarkerImage(imageRed, imageSize); 
-    
-//     // 마커를 생성합니다
-//     var marker = new kakao.maps.Marker({
-//         map: map, // 마커를 표시할 지도
-//         position: positions[i].latlng, // 마커를 표시할 위치
-//         content : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-//         image : markerImage, // 마커 이미지 
-//         clickable: true // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-//     });
-
-//     // 마커에 표시할 인포윈도우를 생성합니다 
-//     var infowindow = new kakao.maps.InfoWindow({
-//         content: positions[i].title // 인포윈도우에 표시할 내용
-//     });
-
-//     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-//     // 이벤트 리스너로는 클로저를 만들어 등록합니다 
-//     // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-//     kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-//     kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-
-// }
-
-
-// // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-// function makeOverListener(map, marker, infowindow) {
-//     return function() {
-//         infowindow.open(map, marker);
-//     };
-// }
-
-// // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
-// function makeOutListener(infowindow) {
-//     return function() {
-//         infowindow.close();
-//     };
-// }
 
