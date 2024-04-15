99a284ad4b6fa586868fc2f7773c8f0c2b6533fb
su9king <su99king@gmail.com>
2024-04-15 19:53:44 +0900

    test

--- index2.js
+++ index2.js
@@ -4,6 +4,11 @@ const range = 'Sheet1'; // 데이터를 읽고 쓸 시트와 범위
 
 let dataList = []; // 데이터 확인 시 사용할 리스트 변수
 
+var clusterer = new kakao.maps.MarkerClusterer({
+    map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+    averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+    minLevel: 10 // 클러스터 할 최소 지도 레벨 
+});
 
 // // 현재 날짜와 시간을 가져오기
 // const currentDate = new Date();
@@ -79,103 +84,18 @@ function readData() {
             dataList.push({name, coords, counts}); // 리스트 변수에 저장
         });
 
-        // 결과를 화면에 표시
-        // const messagesDiv = document.getElementById('messages');
-        // messagesDiv.innerHTML = '';
-        // Object.keys(counts).forEach(coords => {
-        //     messagesDiv.innerHTML += `좌표 ${coords}에는 ${counts[coords]}명이 있습니다.<br>`;
-        // });
-        // console.log(dataList[0].coords);
-
-
-        // dataList 참조하여 마커 생성 후 표시
-        dataList.forEach(element => {
-            let marker = new kakao.maps.Marker({
-                //map: map,
-                position: element.coords,
-                image: markerImage
+        function createMarkersFromDataList() {
+            var markers = dataList.map(function(item) {
+                return new kakao.maps.Marker({
+                    position: item.coords
+                });
             });
-
-            marker.setMap(map);
-
-
-            // 마커 클러스터러를 생성합니다
-            // var clusterer = new kakao.maps.MarkerClusterer({
-            //     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-            //     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-            //     minLevel: 1 // 클러스터 할 최소 지도 레벨
-            // });
-
-            // var markers = element.coords.map(function(i, coords) {
-            //     return new kakao.maps.Marker({
-            //         position : new kakao.maps.LatLng(element.coords)
-            //     });
-            // });
-
-            // clusterer.addMarkers(markers);
-
-
-        });
-
         
-        
-        
-        // 데이터를 가져오기 위해 jQuery를 사용합니다
-        // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-        //$.get("/download/web/data/chicken.json", function(data) {
-            // 데이터에서 좌표 값을 가지고 마커를 표시합니다
-            // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-
             // 클러스터러에 마커들을 추가합니다
-            
+            clusterer.addMarkers(markers);
+        }
 
-
-        // dataList.forEach(element => {
-        //     if (counts[element.coords] >= 4) {
-        //         markerRed.push(element);
-        //     }
-        //     else if (counts[element.coords] >= 3) {
-        //         markerYellow.push(element);
-        //     }
-        //     else if (counts[element.coords] >= 2) {
-        //         markerGreen.push(element);
-        //     }
-        //     else if (counts[element.coords] >= 1) {
-        //         markerBlue.push(element);
-        //     }
-        // });
-
-
-        // dataList 참조하여 마커 생성 후 표시
-        // markerRed.forEach(element => {
-        //     let marker = new kakao.maps.Marker({
-        //         map: map,
-        //         position: element.coords,
-        //         image: markerImageRed
-        //     });
-        
-            // let content = '<div class ="label">'
-            // + '<span class="left"></span><span class="center">'
-            // + counts[element.coords] + " 명이 있어요!" //+ element.population
-            // + '</span><span class="right"></span></div>';
-        
-            // let position = element.coords;
-        
-            // let customOverlay = new kakao.maps.CustomOverlay({
-            //     position: position,
-            //     //content: content,
-            // });
-        
-            // kakao.maps.event.addListener(marker, 'mouseover', function () {
-            //     customOverlay.setMap(map);
-            // });
-        
-            // kakao.maps.event.addListener(marker, 'mouseout', function () {
-            //     setTimeout(function () {
-            //         customOverlay.setMap();
-            //     });
-            // });
-        // });        
     })
+    
     .catch(error => console.error('Error:', error));
 }
