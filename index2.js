42a374b19899470237f9f0f56aa7f928962552e3
su9king <su99king@gmail.com>
2024-04-15 20:59:39 +0900

    test4

--- index2.js
+++ index2.js
@@ -87,34 +87,6 @@ function readData() {
         // console.log(dataList[0].coords);
 
         createMarkersFromDataList();
-        // dataList 참조하여 마커 생성 후 표시
-        dataList.forEach(element => {
-            let marker = new kakao.maps.Marker({
-                //map: map,
-                position: element.coords,
-                image: markerImage
-            });
-
-            marker.setMap(map);
-
-
-            // 마커 클러스터러를 생성합니다
-            // var clusterer = new kakao.maps.MarkerClusterer({
-            //     map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-            //     averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-            //     minLevel: 1 // 클러스터 할 최소 지도 레벨
-            // });
-
-            // var markers = element.coords.map(function(i, coords) {
-            //     return new kakao.maps.Marker({
-            //         position : new kakao.maps.LatLng(element.coords)
-            //     });
-            // });
-
-            // clusterer.addMarkers(markers);
-
-
-        });
 
         
         
